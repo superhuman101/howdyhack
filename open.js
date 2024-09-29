df95613a9e43eeed00de985e598b891dbbc39787
keshap1414 <keshuminie@gmail.com>
2024-09-28 23:51:05 -0500

    added open.css and open.js files

---
+++ open.js
@@ -0,0 +1,4 @@
+// JavaScript to redirect to index.html when the button is clicked
+document.getElementById('enterButton').addEventListener('click', function() {
+    window.location.href = 'index.html'; // Change this to the correct path to your index.html file
+});
